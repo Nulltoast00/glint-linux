e91050504f20b8c9a7217389508d87aed7d48c94
Nulltoast00 <au592648@gmail.com>
2026-06-27 13:20:36 +0530

    Update glint.sh

--- files/system/etc/profile.d/glint.sh
+++ files/system/etc/profile.d/glint.sh
@@ -3,4 +3,4 @@ alias glint-update="rpm-ostree upgrade"
 alias glint-rollback="rpm-ostree rollback"
 alias glint-clean="flatpak uninstall --unused -y && sudo rpm-ostree cleanup -m"
 alias cls="clear"
-fastfetch --logo /usr/share/pixmaps/glint-logo.txt
+fastfetch --logo /usr/share/pixmaps/glint-logo.txt --logo-padding-left 2 --logo-padding-right 4
